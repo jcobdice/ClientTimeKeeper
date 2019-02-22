bdfb5f92ea313e0f2fbbb89f4a3eff67fbe808f0
Dysart <CN0037@mt.gov>
2019-02-22 11:37:49 -0700

    set individual timer displays to show after client is clicked off

--- js/timer.js
+++ js/timer.js
@@ -3,13 +3,17 @@ const clientList = document.querySelector('.client-list');
 clientList.addEventListener('click', timer);
 
 function timer(e) {
-
+// check the card to see if it is already active
     if (e.target.classList == 'client card' && e.target.id != 'active') {
+        // if it isn't start the timer
         startTimer();
+
+        // send the information to the end timer so it can keep count
         endTimer(e.target);
+        // set the id to active so we can find it later
         e.target.id = 'active';
-        console.log(e.target.innerText);
     } else {
+        // stop whatever else was going at the time 
         endTimer(e.target)
     }
     
@@ -31,9 +35,11 @@ function prettyTimeSpent(timeSpent) {
 
 let startTime;
 
+// set a start time
 function startTimer() {
     startTime = new Date();
 }
+
 
 function endTimer(selectedCard) {
     const currentCard = document.querySelector('#active');
@@ -42,43 +48,85 @@ function endTimer(selectedCard) {
     let timeSpent = endTime - startTime; 
 
     if (currentCard != null) {
+        // make sure that we are clicking of the same card we turned on
         if (selectedCard.id == currentCard.id) {
             selectedCard.id = '';
 
-            setTimes(currentCard)
+            // send current card information to setTimes to calculate time past
+            setTimesToLocalStorage(currentCard);
+
+            // print time to card
+            setTimesOnCard(currentCard);
+
 
         } else {
-            setTimes(currentCard);
+            // send current card information to setTimes to calculate time past
+            setTimesToLocalStorage(currentCard);
+
+            // print time to card
+            setTimesOnCard(currentCard);
             currentCard.id = '';
             console.log('Change Task');
         } 
     } else {}
 
-    function setTimes(clientToRecord) {
+    function setTimesToLocalStorage(clientToRecord) {
+        // get clients array from localStorage
         let clients = JSON.parse(localStorage.getItem('clients'));
 
+        // loop through and compare currently selected card to index[name] to update record
+        clients.forEach(function(client){
+
+            let currentClientCard = Array.from(clientToRecord.childNodes);
+            let clientToRecordText = currentClientCard[0].textContent;
+            
+            // if card name matches index[name] apply times to json
+            if(client.name == clientToRecordText){
+                client.recentTime = timeSpent;
+                client.totalTime = client.totalTime + client.recentTime
+            } else {}
+
+
+
+        }); 
+
+        // send data back local storage
+        clients = localStorage.setItem('clients', JSON.stringify(clients));
+    }
+
+    function setTimesOnCard(clientToRecord) {
+        // get clients array from localStorage
+        let clients = JSON.parse(localStorage.getItem('clients'));
+
+        // loop through and compare currently selected card to index[name] to update record
         clients.forEach(function(client){
 
             let time;
 
             let currentClientCard = Array.from(clientToRecord.childNodes);
+            let clientToRecordText = currentClientCard[0].textContent;
 
             let prettyTime = prettyTimeSpent(client.totalTime);
             
+            // set time element on the card for use with prettyTime
             currentClientCard.forEach(function(clientCardElement){
                 if(clientCardElement.className == 'time-span'){
                     time = clientCardElement;
                 }
             });
 
-            if(client.name == clientToRecord.innerText){
-                client.recentTime = timeSpent;
-                client.totalTime = client.totalTime + client.recentTime
+            // if card name matches index[name] apply times to json
+            if(client.name == clientToRecordText){
                 time.innerHTML = prettyTime;
             } else {}
+
+
+
         }); 
 
+        // send data back local storage
         clients = localStorage.setItem('clients', JSON.stringify(clients));
+
     }
 
 }
