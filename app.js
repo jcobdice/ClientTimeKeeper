98c88f6e608bb17f237653ddfaf7240650c5887d
Dysart <CN0037@mt.gov>
2019-03-01 16:20:23 -0700

    Added PWA infrastructure

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 import * as clientManager from './js/clientManager.js';
 import { timer, setTimesOnCard } from './js/timer.js';
+
+window.addEventListener('load', () => {
+    registerServiceWorker();
+});
+
+async function registerServiceWorker() {
+    if ('serviceWorker' in navigator) {
+        try {
+            await navigator.serviceWorker.register('../serviceWorker.js');
+        }   catch (e) {
+        console.log(`Service worker failed to register`);
+        }
+    }
+}
---
+++ serviceWorker.js
@@ -0,0 +1,48 @@
+const cacheName = 'keeptime';
+const staticAssests = [
+    './index.html',
+    './css/style.css',
+    './app.js',
+    './images/clock.svg',
+    './js/clientManager.js',
+    './js/timer.js'
+    ];
+
+self.addEventListener('install', async e => {
+    const cache = await caches.open(cacheName);
+    await cache.addAll(staticAssests);
+    return self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+    const req = e.request;
+    const url = new URL(req.url);
+
+    if (url.origin === location.origin){
+        e.respondWith(cacheFirst(req));
+    } else {
+        e.respondWith(networkAndCache(req));
+    }
+});
+
+async function cacheFirst(req){
+    const cache = await caches.open(cacheName);
+    const cached = await cache.match(req);
+    return cached || fetch(req);
+}
+
+async function networkAndCache(req){
+    const cache = await caches.open(cacheName);
+    try {
+        const fresh = await fetch(req);
+        await cache.put(req, fresh.clone());
+        return fresh;
+    } catch (e) {
+        const cached = await cache.match(req);
+        return cached;
+    }
+}
