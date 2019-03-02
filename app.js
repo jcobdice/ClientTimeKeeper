5aa8e24b6c52170062b9faa96b8356e680e5cd7e
Jacob Dysart <jcobdice@gmail.com>
2019-03-01 21:53:09 -0700

    complete service worker and added prompt listener to allow for local installation. Chrome compatable

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import * as clientManager from './js/clientManager.js';
 import { timer, setTimesOnCard } from './js/timer.js';
 
+let deferredPrompt;
+
 window.addEventListener('load', () => {
     registerServiceWorker();
 });
@@ -14,3 +16,38 @@ async function registerServiceWorker() {
         }
     }
 }
+
+let addApp = document.querySelector('#addApp');
+let btnAdd = document.querySelector('.add');
+let btnCancel = document.querySelector('.cancel');
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  console.log(`before install worked`);
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can add to home screen
+  addApp.style.display = 'block';
+});
+
+btnAdd.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    addApp.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice
+      .then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+  });
+
+btnCancel.addEventListener('click', (e) => {
+    addApp.style.display = 'none';
+});
